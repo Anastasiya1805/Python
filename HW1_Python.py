7a90dee52feabeff6ac1cd747d73822fc0345164
Anastasiya1805 <97237363+Anastasiya1805@users.noreply.github.com>
2022-01-15 13:51:40 +0300

    Update HW1_Python.py

--- HW1_Python.py
+++ HW1_Python.py
@@ -1,47 +1,54 @@
+1) Создать переменную типа String
 a = 'Nastya'
 print(type(a), )
 
+2) Создать переменную типа Integer
 z = 10
 print(type(z), z)
 
+3) Создать переменную типа Float
 f = 5.1233
 print(type(f), f)
 
+4) Создать переменную типа Bytes
 e = bytes(9)
 print(type(e), e)
 
+5)  Создать переменную типа List
 g = [1, 'Nastya']
 print(type(g), g)
 
+6) Создать переменную типа Tuple
 i = tuple()
 print(type(i), i)
 k = tuple('create',)
 print(type(k), k)
 
+7) Создать переменную типа Set
 l = set()
 print(type(l), l)
 l = set('hello')
 print(type(l), l)
-
+ 
+8) Создать переменную типа Frozen set
 m = frozenset('candy')
 print(type(m), m)
 
+9) Создать переменную типа Dict
 n = dict
 n = { }
 n["country"] = "Belarus"
 print(type(n), n)
 
-b = 10
-c = 12
-d = (b*c)
+11) Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные. Вывести в консоль
+b = "Good"
+c = "day"
+d = (b + c)
 print(type(d), d)
 
-v = 5
-o = 'Experience'
-print(type(v), v), print(type(o), o)
+12) Вывести в одну строку переменные типа String и Integer используя “,” 
+print(a, z, sep=",")
 
-j = 7
-m = 'Mir'
-x = str(j) + m
-print(type(x), x)
+13) Вывести в одну строку переменные типа String и Integer используя “+”
+print(a, z, sep="+")
 
