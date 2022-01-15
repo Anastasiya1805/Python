9ed0eba07326f85d810950eefcc36f3b4b78b626
Anastasiya1805 <@anastasiyakolesnikovich1805@mail.ru>
2022-01-15 13:37:36 +0300

    HW1_Python.py

---
+++ HW1_Python.py
@@ -0,0 +1,47 @@
+a = 'Nastya'
+print(type(a), )
+
+z = 10
+print(type(z), z)
+
+f = 5.1233
+print(type(f), f)
+
+e = bytes(9)
+print(type(e), e)
+
+g = [1, 'Nastya']
+print(type(g), g)
+
+i = tuple()
+print(type(i), i)
+k = tuple('create',)
+print(type(k), k)
+
+l = set()
+print(type(l), l)
+l = set('hello')
+print(type(l), l)
+
+m = frozenset('candy')
+print(type(m), m)
+
+n = dict
+n = { }
+n["country"] = "Belarus"
+print(type(n), n)
+
+b = 10
+c = 12
+d = (b*c)
+print(type(d), d)
+
+v = 5
+o = 'Experience'
+print(type(v), v), print(type(o), o)
+
+j = 7
+m = 'Mir'
+x = str(j) + m
+print(type(x), x)
+
